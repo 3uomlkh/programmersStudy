002576e4f8cbcdd83135753b0d58d1c923a44255
3uomlkh <cw1205@duksung.ac.kr>
2024-01-12 23:43:53 +0900

    test commit

---
+++ hyunjin/lv_1/test5.java
@@ -0,0 +1,5 @@
+public class test5 {
+    public static void main(String[] args) {
+        System.out.println("lv_1 문제풀이");
+    }
+}
---
+++ suhyun/lv_1/test6.java
@@ -0,0 +1,5 @@
+public class test6 {
+    public static void main(String[] args) {
+        System.out.println("lv_1 문제풀이");
+    }
+}
