917cee754825598ff8815490328d4d8e2c3f93e6
3uomlkh <cw1205@duksung.ac.kr>
2024-01-15 23:20:12 +0900

    add: a와 b 출력하기

---
+++ chaewon/lv_0/a와_b_출력하기.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class a와_b_출력하기 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int a = scanner.nextInt();
+        int b = scanner.nextInt();
+
+        System.out.println("a = " + a + "\nb = " + b);
+    }
+}
