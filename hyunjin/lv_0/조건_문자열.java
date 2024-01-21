9284eb6122e3fa0d481589d5a9a6f60f3335e4c1
pearhyunjin <pearhyunjin@gmail.com>
2024-01-22 00:05:05 +0900

    add: 조건 문자열

---
+++ hyunjin/lv_0/조건_문자열.java
@@ -0,0 +1,19 @@
+public class 조건_문자열 {
+    public int solution(String ineq, String eq, int n, int m) {
+        if (ineq.equals(">")) {
+            if (eq.equals("=")) {
+                return n >= m ? 1 : 0;
+            } else if (eq.equals("!")) {
+                return n > m ? 1 : 0;
+            }
+        } else if (ineq.equals("<")) {
+            if (eq.equals("=")) {
+                return n <= m ? 1 : 0;
+            } else if (eq.equals("!")) {
+                return n < m ? 1 : 0;
+            }
+        }
+
+        return 0;
+    }
+}
