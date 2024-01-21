11fed0ea8c9ad8d55c1ea0587de9c11904b98eb2
3uomlkh <cw1205@duksung.ac.kr>
2024-01-21 20:33:51 +0900

    add: 홀짝에 따라 다른 값 반환하기

---
+++ chaewon/lv_0/홀짝에_따라_다른_값_반환하기.java
@@ -0,0 +1,22 @@
+public class 홀짝에_따라_다른_값_반환하기 {
+    public static void main(String[] args) {
+        System.out.println(solution(7));
+        System.out.println(solution(10));
+    }
+
+    public static int solution(int n) {
+        int answer = 0;
+
+        if (n % 2 == 0) {
+            for (int i = 2; i <= n; i+=2) {
+                answer += i*i;
+            }
+        } else {
+            for (int i = 1; i <= n; i+=2) {
+                answer += i;
+            }
+        }
+
+        return answer;
+    }
+}
