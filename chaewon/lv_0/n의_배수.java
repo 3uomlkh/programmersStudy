0a517fbfed338da29d92802e54e892faee84c0fd
3uomlkh <cw1205@duksung.ac.kr>
2024-01-21 20:33:22 +0900

    add: n의 배수

---
+++ chaewon/lv_0/n의_배수.java
@@ -0,0 +1,10 @@
+public class n의_배수 {
+    public static void main(String[] args) {
+        System.out.println(solution(98,2));
+        System.out.println(solution(34,3));
+    }
+
+    public static int solution(int num, int n) {
+        return (num % n == 0) ? 1 : 0;
+    }
+}
