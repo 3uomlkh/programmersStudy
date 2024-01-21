232701df90c2a401f72316e0fc1a3c566e5d4531
3uomlkh <cw1205@duksung.ac.kr>
2024-01-21 16:26:48 +0900

    add: 문자열 곱하기

---
+++ chaewon/lv_0/문자열_곱하기.java
@@ -0,0 +1,10 @@
+public class 문자열_곱하기 {
+    public static void main(String[] args) {
+        System.out.println(solution("string", 3));
+        System.out.println(solution("love", 10));
+    }
+
+    public static String solution(String my_string, int k) {
+        return my_string.repeat(k);
+    }
+}
