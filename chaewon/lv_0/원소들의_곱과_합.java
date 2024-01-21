599af5c1a59e17512ef315d318968f4648b9356e
3uomlkh <cw1205@duksung.ac.kr>
2024-01-21 21:56:47 +0900

    add: 원소들의 곱과 합

---
+++ chaewon/lv_0/원소들의_곱과_합.java
@@ -0,0 +1,18 @@
+public class 원소들의_곱과_합 {
+    public static void main(String[] args) {
+        System.out.println(solution(new int[] {3, 4, 5, 2, 1}));
+        System.out.println(solution(new int[] {5, 7, 8, 3}));
+    }
+
+    public static int solution(int[] num_list) {
+        int a = 1;
+        int b = 0;
+
+        for (int i : num_list) {
+            a *= i;
+            b += i;
+        }
+
+        return (a < b*b) ? 1 : 0;
+    }
+}
