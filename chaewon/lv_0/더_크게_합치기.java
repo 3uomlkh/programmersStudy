2d2da5ba002f428484714a7fbf5b27272d1e9954
3uomlkh <cw1205@duksung.ac.kr>
2024-01-21 16:27:00 +0900

    add: 더 크게 합치기

---
+++ chaewon/lv_0/더_크게_합치기.java
@@ -0,0 +1,16 @@
+public class 더_크게_합치기 {
+    public static void main(String[] args) {
+        System.out.println(solution(9,91));
+        System.out.println(solution(89,8));
+    }
+
+    public static int solution(int a, int b) {
+        String s1 = "" + a + b;
+        String s2 = "" + b + a;
+
+        int i1 = Integer.parseInt(s1);
+        int i2 = Integer.parseInt(s2);
+
+        return (i1 > i2 || i1 == i2) ? i1 : i2;
+    }
+}
