9907ec5ce2b4ebe2e8fceaf4ab6c95af3cdb9384
3uomlkh <cw1205@duksung.ac.kr>
2024-01-23 22:56:51 +0900

    add: 플리츠 수열 만들기

---
+++ chaewon/lv_0/콜라츠_수열_만들기.java
@@ -0,0 +1,24 @@
+import java.util.ArrayList;
+
+public class 콜라츠_수열_만들기 {
+    public static void main(String[] args) {
+        System.out.println(solution(10));
+    }
+
+    public static ArrayList<Integer> solution(int n) {
+        ArrayList<Integer> numbers = new ArrayList<>();
+        numbers.add(n);
+
+        while (n != 1) {
+            if (n % 2 == 0) {
+                n = n/2;
+                numbers.add(n);
+            } else {
+                n = 3 * n + 1;
+                numbers.add(n);
+            }
+        }
+
+        return numbers;
+    }
+}
