d4c50a10650dad7cf320864b6fbf83c1673df091
3uomlkh <cw1205@duksung.ac.kr>
2024-01-25 15:57:53 +0900

    add: 배열 만들기 5

---
+++ chaewon/lv_0/배열_만들기_5.java
@@ -0,0 +1,19 @@
+import java.util.ArrayList;
+
+public class 배열_만들기_5 {
+    public static void main(String[] args) {
+        String[] intStrs = {"0123456789","9876543210","9999999999999"};
+        System.out.println(solution(intStrs, 50000, 5, 5));
+    }
+
+    public static ArrayList<Integer> solution(String[] intStrs, int k, int s, int l) {
+        ArrayList<Integer> answer = new ArrayList<>();
+
+        for (String intStr : intStrs) {
+            int a = Integer.parseInt(intStr.substring(s, s + l));
+            if (a > k) answer.add(a);
+        }
+
+        return answer;
+    }
+}
