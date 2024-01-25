0d2eb1ff788b5dd739687c6f89c5c088d42fb503
3uomlkh <cw1205@duksung.ac.kr>
2024-01-25 17:40:40 +0900

    add: 문자열 뒤집기

---
+++ chaewon/lv_0/문자열_뒤집기.java
@@ -0,0 +1,13 @@
+public class 문자열_뒤집기 {
+    public static void main(String[] args) {
+        System.out.println(solution("Progra21Sremm3", 6, 12));
+    }
+
+    public static String solution(String my_string, int s, int e) {
+        StringBuilder sb = new StringBuilder();
+
+        sb.append(my_string.substring(s, e + 1));
+        sb.reverse();
+        return my_string.substring(0, s) + sb + my_string.substring(e + 1);
+    }
+}
