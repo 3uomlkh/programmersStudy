79edabe2336b937864d431168e4b4171a5d48bb6
3uomlkh <cw1205@duksung.ac.kr>
2024-01-28 23:58:28 +0900

    add: n보다 커질 때까지 더하기

---
+++ chaewon/lv_0/n보다_커질_때까지_더하기.java
@@ -0,0 +1,20 @@
+public class n보다_커질_때까지_더하기 {
+    public static void main(String[] args) {
+        System.out.println(solution(new int[] {34, 5, 71, 29, 100, 34}, 123));
+    }
+
+    public static int solution(int[] numbers, int n) {
+        int answer = 0;
+        int result = 0;
+
+        for (int i = 0; i < numbers.length; i++) {
+            result += numbers[i];
+            if (result > n) {
+                answer = result;
+                break;
+            }
+        }
+
+        return answer;
+    }
+}
