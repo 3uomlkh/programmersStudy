b6df5db40468c52050253471139a27a3a5875706
pearhyunjin <pearhyunjin@gmail.com>
2024-01-30 13:42:04 +0900

    add: 가까운 1 찾기

---
+++ hyunjin/lv_0/가까운_1_찾기.java
@@ -0,0 +1,16 @@
+public class 가까운_1_찾기 {
+    public int solution(int[] arr, int idx) {
+        int answer = 0;
+
+        for (int i=idx; i<arr.length; i++) {
+            if (arr[i] == 1) {
+                answer = i;
+                break;
+            } else if (arr[i] != 1) {
+                answer = -1;
+            }
+        }
+
+        return answer;
+    }
+}
