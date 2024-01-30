8534ee0116c20f6729711c6d926b97eb3e8c70fa
pearhyunjin <pearhyunjin@gmail.com>
2024-01-31 02:23:34 +0900

    add: 소문자로 바꾸기

---
+++ hyunjin/lv_0/소문자로_바꾸기.java
@@ -0,0 +1,15 @@
+public class 소문자로_바꾸기 {
+    public String solution(String myString) {
+        String answer = "";
+
+        for (int i=0; i<myString.length(); i++) {
+            if ((int)myString.charAt(i) <= 90 && (int)myString.charAt(i) >= 65) {
+                answer += (char)((int)myString.charAt(i)+32);
+            } else {
+                answer += myString.charAt(i);
+            }
+        }
+
+        return answer;
+    }
+}
