cdd620653437bcd4faf29442ad88c5065fc6fc71
pearhyunjin <pearhyunjin@gmail.com>
2024-01-30 23:57:34 +0900

    fix: 배열 만들기 3
    반복되는 부분 한번만 출력되는 오류 수정

--- hyunjin/lv_0/배열_만들기_3.java
+++ hyunjin/lv_0/배열_만들기_3.java
@@ -2,24 +2,16 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class 배열_만들기_3 {
-    public List<Integer> solution(int[] arr, int[][] intervals) {
-        int oneA = intervals[0][0];
-        int oneB = intervals[0][1];
-        int twoA = intervals[1][0];
-        int twoB = intervals[1][1];
-        List<Integer> answer = new ArrayList<>();
+    public int[] solution(int[] arr, int[][] intervals) {
+        List<Integer> list = new ArrayList<>();
+        for (int i = 0; i < intervals.length; i++) {
+            int start = intervals[i][0];
+            int end = intervals[i][1];
 
-        for (int i=0; i<arr.length; i++) {
-            if (i >= oneA && i <= oneB) {
-                answer.add(i-oneA, arr[i]);
+            for (int j = start; j <= end; j++) {
+                list.add(arr[j]);
             }
         }
-        for (int j=0; j<arr.length; j++) {
-            if (j >= twoA && j<= twoB) {
-                answer.add(j-twoA+oneB, arr[j]);
-            }
-        }
-
-        return answer;
+        return list.stream().mapToInt(i->i).toArray();
     }
 }
