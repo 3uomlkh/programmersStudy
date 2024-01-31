b9562eb75f6412c83ffc6226cb5f30bc60626799
pearhyunjin <pearhyunjin@gmail.com>
2024-01-31 22:41:08 +0900

    add: rny_string

---
+++ hyunjin/lv_0/rny_string.java
@@ -0,0 +1,9 @@
+public class rny_string {
+    public String solution(String rny_string) {
+        String answer = "";
+
+        answer = rny_string.replaceAll("m", "rn");
+
+        return answer;
+    }
+}
