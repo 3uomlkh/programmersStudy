1e619b823075149191d01d0f04305bc1c89bf740
3uomlkh <cw1205@duksung.ac.kr>
2024-01-31 23:44:41 +0900

    add: 정수 부분

---
+++ chaewon/lv_0/정수_부분.java
@@ -0,0 +1,12 @@
+package lv_0;
+
+public class 정수_부분 {
+    public static void main(String[] args) {
+        System.out.println(solution(1.42));
+        System.out.println(solution(69.32));
+    }
+
+    public static int solution(double flo) {
+        return (int) flo;
+    }
+}
