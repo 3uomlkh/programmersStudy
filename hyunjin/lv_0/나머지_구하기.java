a54fae5f40bafa1a77df3967dfb8cd0b1c106d8c
pearhyunjin <pearhyunjin@gmail.com>
2024-02-05 00:36:54 +0900

    add: 입문 Day3

---
+++ hyunjin/lv_0/나머지_구하기.java
@@ -0,0 +1,15 @@
+public class 나머지_구하기 {
+
+    public static void main(String[] args) {
+        System.out.println(solution(3, 2));
+        System.out.println(solution(10, 5));
+    }
+
+    public static int solution(int num1, int num2) {
+        int answer = -1;
+
+        answer = num1 % num2;
+
+        return answer;
+    }
+}
