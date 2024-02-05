85b42b38028a6555e3a2c4a40f2ff6610bddfbcc
pearhyunjin <pearhyunjin@gmail.com>
2024-02-05 23:22:29 +0900

    add: 입문 Day5

---
+++ hyunjin/lv_0/아이스_아메리카노.java
@@ -0,0 +1,17 @@
+import java.util.Arrays;
+
+public class 아이스_아메리카노 {
+    public static void main(String[] args) {
+        System.out.println(Arrays.toString(solution(5500)));
+        System.out.println(Arrays.toString(solution(15000)));
+    }
+
+    public static int[] solution(int money) {
+        int[] answer = new int[2];
+
+        answer[0] = money / 5500;
+        answer[1] = money % 5500;
+
+        return answer;
+    }
+}
