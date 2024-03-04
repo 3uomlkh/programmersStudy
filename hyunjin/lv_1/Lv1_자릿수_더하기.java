d5756b991192a7ff957ca3c5619a37a3abb2a305
pearhyunjin <pearhyunjin@gmail.com>
2024-03-04 20:02:21 +0900

    add: Lv.1 Day1 5문제

---
+++ hyunjin/lv_1/Lv1_자릿수_더하기.java
@@ -0,0 +1,17 @@
+public class Lv1_자릿수_더하기 {
+    public static void main(String[] args) {
+        System.out.println(solution(123));
+        System.out.println(solution(987));
+    }
+
+    public static int solution(int n) {
+        int answer = 0;
+
+        while (n > 0) {
+            answer += n % 10;
+            n /= 10;
+        }
+
+        return answer;
+    }
+}
---
+++ hyunjin/lv_1/평균_구하기.java
@@ -0,0 +1,16 @@
+public class 평균_구하기 {
+    public static void main(String[] args) {
+        System.out.println(solution(new int[]{1, 2, 3, 4}));
+        System.out.println(solution(new int[]{5, 5}));
+    }
+
+    public static double solution(int[] arr) {
+        double answer = 0;
+
+        for (int i = 0; i < arr.length; i++) {
+            answer += arr[i];
+        }
+
+        return answer / arr.length;
+    }
+}
