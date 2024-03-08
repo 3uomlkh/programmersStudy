e33903d865b381822ed54dd28d5baf6f02a5d0ed
3uomlkh <cw1205@duksung.ac.kr>
2024-03-08 11:06:37 +0900

    add: Lv.1 5문제

---
+++ chaewon/lv_1/수박수박수박수박수박수.java
@@ -0,0 +1,19 @@
+package lv_1;
+
+public class 수박수박수박수박수박수 {
+    public static void main(String[] args) {
+        System.out.println(solution(3));
+        System.out.println(solution(4));
+    }
+
+    public static String solution(int n) {
+        StringBuilder answer = new StringBuilder();
+
+        for (int i = 0; i < n; i++) {
+            if (i % 2 == 0) answer.append("수");
+            else answer.append("박");
+        }
+
+        return answer.toString();
+    }
+}
