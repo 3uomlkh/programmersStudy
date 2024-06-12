14cad66c3795743d7c634d1a865b117d0fcdb28f
3uomlkh <cw1205@duksung.ac.kr>
2024-06-12 20:19:44 +0900

    add: Lv.2 1문제

---
+++ chaewon/lv_2/피로도.java
@@ -0,0 +1,31 @@
+package lv_2;
+
+public class 피로도 {
+    static boolean[] visit;
+    static int answer;
+    public static void main(String[] args) {
+        int k = 80;
+        int[][] dungeons = {{80,20},{50,40},{30,10}};
+        System.out.println(solution(k, dungeons));
+    }
+
+    public static int solution(int k, int[][] dungeons) {
+        int depth = 0;
+        visit = new boolean[dungeons.length];
+
+        dfs(depth, k, dungeons);
+
+        return answer;
+    }
+
+    public static void dfs(int depth, int k, int[][] dungeons) {
+        for (int i = 0; i < dungeons.length; i++) {
+            if (!visit[i] && k >= dungeons[i][0]) {
+                visit[i] = true;
+                dfs(depth + 1, k - dungeons[i][1], dungeons);
+                visit[i] = false;
+            }
+        }
+        answer = Math.max(answer, depth);
+    }
+}
